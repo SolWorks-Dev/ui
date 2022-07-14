d47f4fff2612560481a88ca6fd47fe331ca96b4d
zhe-t <106411133+zhe-t@users.noreply.github.com>
2022-07-14 21:24:00 +0100

    Remove surrounding div from logo component

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -3,18 +3,17 @@ import React, { FC } from "react";
 export interface LogoProps {
     logoUrl: string;
     altText: string;
+    sizePx?: number;
 };
 
-export const Logo: FC<LogoProps> = ({logoUrl, altText}) => {
+export const Logo: FC<LogoProps> = ({logoUrl, altText, sizePx = 78}) => {
     return (
-        <div className="mac-logo">
-            <img
-                src={logoUrl}
-                width="78px"
-                height="78px"
-                alt={`${altText} logo`}
-                style={{borderRadius: '40px'}}
-            />
-        </div>
+        <img
+            src={logoUrl}
+            width={`${sizePx}px`}
+            height={`${sizePx}px`}
+            alt={`${altText} logo`}
+            style={{borderRadius: '40px'}}
+        />
     );
 };

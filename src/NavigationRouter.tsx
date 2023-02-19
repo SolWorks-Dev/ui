7ce364b280b5f8645012194f4adaf3783b037f83
zhe-t <106411133+zhe-t@users.noreply.github.com>
2023-02-19 22:14:18 +0000

    Add 404 view

--- src/NavigationRouter.tsx
+++ src/NavigationRouter.tsx
@@ -3,6 +3,7 @@ import { HomeView } from './views/HomeView';
 import { Routes, Route } from 'react-router-dom';
 import { ApplicationPage } from './components/ApplicationPage/ApplicationPage';
 import { CategoryView } from './views/CategoryView';
+import { FourZeroFourView } from './views/FourZeroFourView';
 
 
 export default function NavigationRouter() {
@@ -10,5 +11,6 @@ export default function NavigationRouter() {
     <Route path="/" element={<HomeView />} />
     <Route path="apps/:id" element={<ApplicationPage />} />
     <Route path="category/:id" element={<CategoryView />} />
+    <Route path="*" element={<FourZeroFourView />} />
   </Routes>;
 }
